2b26e265e03da65d4574df5a415973ff3a7d4a80
lnnaf <vanlinh12b5@gmail.com>
2020-12-25 00:32:56 +0700

    practice for angular overview

---
+++ 03.angular-overview/practice/practice/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {PetComponent} from "./pet/pet.component";
+
+
+const routes: Routes = [
+  {path: 'pet', component: PetComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
